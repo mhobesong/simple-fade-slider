c46ae125b8c96ab84c573ddee785969e35e7e133
besong <mosbesong@gmail.com>
2018-12-19 02:33:52 +0100

    show selected image

--- jasmine/spec/SliderSpec.js
+++ jasmine/spec/SliderSpec.js
@@ -13,7 +13,18 @@ describe("FadeViewer", function() {
 	});
 
 	it("should load images into a view container", function() {
-		var images = document.querySelectorAll('.fadeviewer .fadeviewer-images img');
+		var images = document.querySelectorAll('.fadeviewer .fadeviewer-images .fadeviewer-image');
 		expect(images.length).toEqual(4);
+	});
+
+	it("show selected image", function() {
+		fadeViewer.show(2);
+		var images = document.querySelectorAll('.fadeviewer .fadeviewer-images .fadeviewer-image');
+		for (var i=0; i < images.length; i++){
+			if (i == 2)
+				expect(images[i].style.display).toEqual('block');
+			else
+				expect(images[i].style.display).toEqual('none');
+		};
 	});
 });
