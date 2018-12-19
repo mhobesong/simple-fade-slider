41b97b6233858d0d2bfd13a08b7718a6e9471b50
besong <mosbesong@gmail.com>
2018-12-19 17:09:24 +0100

    Fix fadein/fadeout effect

--- jasmine/spec/SliderSpec.js
+++ jasmine/spec/SliderSpec.js
@@ -1,10 +1,12 @@
 var fadeViewer = null;
 describe("FadeViewer", function() {
 	beforeEach(function() {
+		fadeViewer = FadeViewer('list');
+	});
+
+	afterEach(function() {
 		if (document.querySelector('.fadeviewer'))
 			document.querySelector('.fadeviewer').remove();
-
-		fadeViewer = FadeViewer('list');
 	});
 
 	it("should create fadeviewer container", function() {
@@ -17,14 +19,84 @@ describe("FadeViewer", function() {
 		expect(images.length).toEqual(4);
 	});
 
-	it("show selected image", function() {
+	it("should show selected image", function(done) {
 		fadeViewer.show(2);
 		var images = document.querySelectorAll('.fadeviewer .fadeviewer-images .fadeviewer-image');
-		for (var i=0; i < images.length; i++){
-			if (i == 2)
-				expect(images[i].style.display).toEqual('block');
-			else
-				expect(images[i].style.display).toEqual('none');
-		};
+		setTimeout(function(){
+			for (var i=0; i < images.length; i++){
+				if (i == 2)
+					expect(parseFloat(images[i].style.opacity) > 0).toEqual(true);
+				else
+					expect(images[i].style.opacity < 1).toEqual(true);
+			};
+			done();
+		},100);
+	});
+
+	it("should Loop to first image if selected image out of bound", function(done) {
+		fadeViewer.show(4);
+		var images = document.querySelectorAll('.fadeviewer .fadeviewer-images .fadeviewer-image');
+
+		setTimeout(function(){
+			for (var i=0; i < images.length; i++){
+				if (i == 0)
+					expect(parseFloat(images[i].style.opacity) > 0).toEqual(true);
+				else
+					expect(images[i].style.opacity < 1).toEqual(true);
+			};
+		},100);
+
+		fadeViewer.show(-1);
+		images = document.querySelectorAll('.fadeviewer .fadeviewer-images .fadeviewer-image');
+
+		setTimeout(function(){
+			for (var i=0; i < images.length; i++){
+				if (i == 3)
+					expect(parseFloat(images[i].style.opacity) > 0).toEqual(true);
+				else
+					expect(images[i].style.opacity < 1).toEqual(true);
+			};
+			done();
+		},200);
+	});
+
+	it("should show next image", function(done) {
+		fadeViewer.show(1);
+
+		setTimeout(function(){
+			fadeViewer.next();
+
+			var images = document.querySelectorAll('.fadeviewer .fadeviewer-images .fadeviewer-image');
+
+			setTimeout(function(){
+				for (var i=0; i < images.length; i++){
+					if (i == 2)
+						expect(parseFloat(images[i].style.opacity) > 0).toEqual(true);
+					else
+						expect(parseFloat(images[i].style.opacity) < 1).toEqual(true);
+					done();
+				};
+			},100);
+		},300);
+	});
+
+	it("should show previous image", function(done) {
+		fadeViewer.show(3);
+
+		setTimeout(function(){
+			fadeViewer.prev();
+
+			var images = document.querySelectorAll('.fadeviewer .fadeviewer-images .fadeviewer-image');
+
+			setTimeout(function(){
+				for (var i=0; i < images.length; i++){
+					if (i == 2)
+						expect(parseFloat(images[i].style.opacity) > 0).toEqual(true);
+					else
+						expect(parseFloat(images[i].style.opacity) < 1).toEqual(true);
+					done();
+				};
+			},100);
+		},300);
 	});
 });
--- src/simple-fade-slider.js
+++ src/simple-fade-slider.js
@@ -20,10 +20,19 @@ function FadeViewer(){
 		var container = document.createElement('div');
 		container.setAttribute('class', 'fadeviewer-images');
 		document.querySelectorAll('#'+args[0]+' li img').forEach(function(img){
-			var elem = new Image();
-			elem.src = img.src;
-
-			container.appendChild(elem);
+			var div = document.createElement('div');
+			div.setAttribute('class', 'fadeviewer-image');
+			div.style.backgroundImage = 'url("'+img.src+'")';
+			div.style.backgroundSize = 'contain';
+			div.style.backgroundPosition = 'center';
+			div.style.backgroundRepeat = 'no-repeat';
+			div.style.width = '100%';
+			div.style.height = '100%';
+			div.style.position = 'absolute';
+			div.style.opacity = '1';
+			div.style.top = 0;
+			div.style.left=0
+			container.appendChild(div);
 		});
 		document.getElementById(id).appendChild(container);
 	}
@@ -33,19 +42,83 @@ function FadeViewer(){
 
 		images.forEach(function(image){
 			image.style.position = 'absolute';
-			image.style.width = '100%';
-			image.style.height = '100%';
 		});
 
 		var container = document.querySelector('#'+id+' .fadeviewer-images');
-		container.style.position = 'relative';
+		container.style.position = 'fixed';
+		container.style.top = '0';
+		container.style.left = '0';
 		container.style.width = '100vw';
 		container.style.height = '100vh';
+	}
+
+	function show(index){
+		var images = document.querySelectorAll('#'+id+' .fadeviewer-images .fadeviewer-image');
+
+		if (index >= images.length) index = 0;
+		if (index < 0) index = images.length - 1;
+
+		for (var i=0; i < images.length; i++){
+			if (i == index){
+				//images[i].style.opacity = 1;
+				fadein(images[i]);
+			}
+			else{
+				fadeout(images[i]);
+				//images[i].style.opacity = 0;
+			}
+		}
+	}
+
+	function fadeout(container){
+		var timer = setInterval(function(){
+			container.style.opacity = parseFloat(container.style.opacity) - 0.1;
+			if(parseFloat(container.style.opacity) < 0) {
+				clearInterval(timer);
+				container.style.opacity = '0'
+			}
+		},50);
+	}
+
+	function fadein(container){
+		var timer = setInterval(function(){
+			container.style.opacity = parseFloat(container.style.opacity) + 0.1;
+			if(parseFloat(container.style.opacity) >= 1){
+				clearInterval(timer);
+				container.style.opacity = '1'
+			}
+		},50);
+	}
+
+	function next(){
+		var images = document.querySelectorAll('#'+id+' .fadeviewer-images .fadeviewer-image');
+		
+		for (var i=0; i < images.length; i++){
+			if(images[i].style.opacity == 1){
+				show(i+1);
+				break;
+			}
+		}
+	}
+
+	function prev(){
+		var images = document.querySelectorAll('#'+id+' .fadeviewer-images .fadeviewer-image');
+		
+		for (var i=0; i < images.length; i++){
+			if(images[i].style.opacity == 1){
+				show(i-1);
+				break;
+			}
+		}
 	}
 
 	createRootContainer();
 	loadImages();
 	stackImages();
 
-	return {};
+	return {
+		show:show,
+		next:next,
+		prev:prev
+	};
 }
